c35d34cc2ddefa1bba03b3a2b40780ab403c2694
Mohamed Elbehairy <mohamed.elbehairy@robustastudio.com>
2020-11-07 20:06:34 +0200

    chore: setup folder structure and react router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
 import client from './apollo-client';
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 
   document.getElementById('root')
 );
